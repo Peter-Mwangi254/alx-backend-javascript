e05d9b205461b11f5720735a36c1b887e6a2f69e
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-27 20:38:29 +0000

    created a function that returns the value of the previous function

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  for (const [departmentName, employees] of Object.entries(employeesList)) {
+    allEmployees[departmentName] = [...employees];
+  }
+
+  return {
+    allEmployees,
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
