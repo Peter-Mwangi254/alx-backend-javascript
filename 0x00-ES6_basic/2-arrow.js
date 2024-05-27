6ba649b1c5f836ad5fd16b6c3db3c93c144fa4cc
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-27 18:27:14 +0000

    Arrow functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
