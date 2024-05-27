c11cc2860e1dfd7e793a3fb9606459f8781a1e5c
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-27 19:58:00 +0000

    ES6 method properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+/* eslint-disable import/extensions */
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
