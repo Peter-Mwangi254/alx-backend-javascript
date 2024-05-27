3b09f2112d7eb800a44c7a3b5b54336954ab7d54
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-27 20:21:36 +0000

    ES6’s for...of operator.

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 /* eslint-disable guard-for-in */
 /* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
-  for (let [idx, value] of array.entries()) {
+  for (const [idx, value] of array.entries()) {
     array[idx] = appendString + value;
   }
 
