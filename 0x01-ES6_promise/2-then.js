d1ea6ead226714893051cf3fb3bb0b983e69c321
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-29 10:31:13 +0000

    error handling

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+/* eslint-disable no-console */
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
+}
