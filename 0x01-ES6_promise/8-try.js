25803f0af5c69e8ced04a4fa4399504af1441d48
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-29 21:18:30 +0000

    Throw error / try catch

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,1 +1,6 @@
-#!/usr/bin/node
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
