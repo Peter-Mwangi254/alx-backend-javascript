0cbca074ca7ad05e4c995bd0b54deebaa488c0b5
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-30 18:52:52 +0000

    class that returns an array

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const rooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+  return rooms;
+}
