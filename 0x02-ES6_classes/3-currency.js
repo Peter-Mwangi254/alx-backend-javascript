12f8f719ada81f01011b888eabe485cf03b44743
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-30 19:19:24 +0000

    add currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(n) {
+    this._name = n;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(c) {
+    this._name = c;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
