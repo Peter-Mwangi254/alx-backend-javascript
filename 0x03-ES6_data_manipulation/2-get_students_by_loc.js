e86c745941587c58281ceedb144ab42b726dfafc
Peter-Mwangi254 <pm6093628@gmail.com>
2024-06-03 19:15:52 +0000

    filter

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(list, city) {
-  return list.filter((student) => student.location === city);
+export default function getStudentsByLocation(studentList, city) {
+  return studentList.filter((student) => student.location === city);
 }
