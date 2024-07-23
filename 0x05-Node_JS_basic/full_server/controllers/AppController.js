5d97cd8d8fb78263eb6cf2d28f8ad7d5516249e5
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-23 14:28:30 -0700

    Organized a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+class AppController {
+  static getHomepage(request, response) {
+    response.send(200, 'Hello Holberton School!');
+  }
+}
+
+export default AppController;
--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+import express from 'express';
+import controllerRouting from './routes/index';
+
+const app = express();
+const port = 1245;
+
+controllerRouting(app);
+
+app.listen(port, () => {});
+
+export default app;
