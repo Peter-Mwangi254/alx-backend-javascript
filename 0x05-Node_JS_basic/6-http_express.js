2f51e03df69f66059d8c9db55f86488e59d1a512
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-23 14:07:45 -0700

    Created a small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+});
+
+module.exports = app;
