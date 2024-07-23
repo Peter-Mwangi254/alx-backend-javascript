1d272cdf102824571fa4ca28f5e98b4046611db4
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-23 02:09:09 -0700

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+process.stdin.setEncoding('utf-8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
