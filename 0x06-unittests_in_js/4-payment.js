f32f8c6f2c1f158606d085da3a38fd77cc64b6e5
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-25 13:19:37 -0700

    using Stubs

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+Utils = require('./utils');
+
+Utils = require('./utils');
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const output = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${output}`);
+}
+
+module.exports = sendPaymentRequestToApi;
