ccb107831483535fb75dde8f56b68049f7a4d693
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-25 12:57:02 -0700

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,46 +1,47 @@
-const assert = require('assert');
-const calculateNumber = require('./1-calcul');
+var chai = require('chai');
+var expect = chai.expect;
+const calculateNumber = require('./2-calcul_chai');
 
 describe("Tests calculateNumber function:", function() {
   describe("Calculates two integers:", function() {
     it('Returns 6 for SUM', function() {
-      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
 
     it('Returns -4 for SUBTRACT', function() {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     });
 
     it('Returns 0.2 for DIVIDE', function() {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     });
   });
 
   describe("Calculates one float & one integer:", function() {
     it('Returns -3 for SUBTRACT', function() {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 4), -3);
+      expect(calculateNumber('SUBTRACT', 1.2, 4)).to.equal(-3);
     });
   });
 
   describe("Calculates one float & one integer (round down):", function() {
     it('Returns 4 for SUM', function() {
-      assert.strictEqual(calculateNumber('SUM', 1.3, 2.7), 4);
+      expect(calculateNumber('SUM', 1.3, 2.7)).to.equal(4);
     });
   });
 
   describe("Calculates one float & one int reversed:", function() {
     it('Returns 3 for SUBTRACT', function() {
-      assert.strictEqual(calculateNumber('SUBTRACT', 4.2, 1), 3);
+      expect(calculateNumber('SUBTRACT', 4.2, 1)).to.equal(3);
     });
   });
 
   describe("Calculates two floats:", function() {
     it('Returns 6 for SUM', function() {
-      assert.strictEqual(calculateNumber('SUM', 1.5, 3.5), 6);
+      expect(calculateNumber('SUM', 2.4, 3.6)).to.equal(6);
     });
 
     it('Returns -2 for SUBTRACT', function() {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.5), -2);
+      expect(calculateNumber('SUBTRACT', 2.4, 4.4)).to.equal(-2);
     });
   });
 });
