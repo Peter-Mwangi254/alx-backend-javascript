b13947e0cf527a5bc8b3ab0481ce34931212d9cc
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-25 13:09:33 -0700

    using Spies

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+const Utils = require('./utils');
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${sum}`);
+}
+
+module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+var { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('Spy', function () {
+  it('Ensures math is the same.', () => {
+    const spyUtils = sinon.spy(Utils, 'calculateNumber');
+    const spyConsole = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spyUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
+
+    spyUtils.restore();
+    spyConsole.restore();
+  });
+});
